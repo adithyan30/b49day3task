4355a27cff401b3599dcb0dc4731b92d1dc98b23
adithyan30 <127578276+adithyan30@users.noreply.github.com>
2023-03-23 10:44:15 +0530

    Update task.js

--- task.js
+++ task.js
@@ -13,24 +13,3 @@ xhr.onload = function() {
 
 
 
-
-var abc = new XMLHttpRequest();
-abc.open('GET','https://api.publicapis.org/entries');
-abc.responseType = 'json';
-abc.send();
-abc.onload = function() {
-    var responseobj = abc.response;
-    console.log(responseobj)
-
-    for (var i = 0; i < responseobj.entries.length; i++){
-        console.log(responseobj.entries[i].Description)
-
-    }
-}
-
-
-var name = "adithyan";
-var age = 28;
-var designation = "job seeker";
-var address = "virudhachalam";
-//console.log(`my name is ${name} and my age is ${age} . I AM ${designation} AND LIVING IN ${address}`)
